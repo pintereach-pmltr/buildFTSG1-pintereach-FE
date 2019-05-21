a34a3f47dfefa738b4a2b553914931e70efbe4c0
josh cooter <thejoshcooter@gmail.com>
2019-05-20 20:23:37 -0500

    hookup redux

--- pintereach/src/index.js
+++ pintereach/src/index.js
@@ -1,8 +1,24 @@
+// react import
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// react router imports
+import { BrowserRouter as Router } from 'react-router-dom';
+// redux imports
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers';
+// component imports
 import App from './App';
 
+// redux store
+const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <Provider store={store}>
+        <App />
+    </Provider>
+</Router>,
+document.getElementById('root')
+);
 
---
+++ pintereach/src/store/reducers/index.js
@@ -0,0 +1,10 @@
+// intitial state
+const initialState = {
+    init: 'hello'
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
