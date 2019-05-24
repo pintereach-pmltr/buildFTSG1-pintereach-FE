42638cd226bee79cd5e19845a80dd93cef15f450
josh cooter <thejoshcooter@gmail.com>
2019-05-23 23:12:43 -0500

    add all pins view

--- pintereach/src/views/dashboard/Pins.js
+++ pintereach/src/views/dashboard/Pins.js
@@ -19,14 +19,48 @@ const PinsContent = styled.div`
     margin: 0 auto;
 `
 
+const ArticlePin = styled.div`
+    background-color: green;
+    font-size: 1.25rem;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+    margin: 1rem 0;
+    padding: 0.5rem;
+    border-radius: 10px;
+    background-color: #2B2D42;
+    color: #fff;
+`
+
 class Pins extends Component {
     render() {
+        console.log('[CHECK PINS STATE]', this.props);
         return (
             <PinsContainer>
                 <PinsContent>
-                    {this.props.pins.map(pin => {
-                        return <Pin key={pin.id} name={pin.article_label} url={pin.url} boardId={pin.board_id} />
+                <div>
+            {this.props.boards.map(board => {
+            if (board.articles) {
+                return (
+                <div>
+                    {board.articles.map((article, articleIndex) => {
+                    if (article.board_id) {
+                    return (
+                        <ArticlePin key={`article = ${articleIndex}`}>
+                            <span>{article.article_label}</span>
+                            <a target="_blank" href={article.url.toString()}>{article.url}</a>
+                            <span>{article.board_id}</span>
+                            <button onClick={() =>this.deleteArticle(article.id)}>Delete</button>
+                        </ArticlePin>
+                    );
+                    }
                     })}
+                </div>
+                );
+            }
+            })}
+            </div>
                 </PinsContent>
             </PinsContainer>
         );
@@ -35,7 +69,7 @@ class Pins extends Component {
 
 const mapStateToProps = state => {
     return {
-        pins: state.articles
+        boards: state.boards
     }
 };
 
