002f180e44ccf66a144e9857d72e1e84dce631e7
josh cooter <thejoshcooter@gmail.com>
2019-05-23 21:17:12 -0500

    adding pins and linking

--- pintereach/src/views/dashboard/BoardView.js
+++ pintereach/src/views/dashboard/BoardView.js
@@ -2,6 +2,34 @@
 import React, { Component } from 'react';
 // redux imports
 import { connect } from 'react-redux';
+// react router imports
+import { NavLink } from 'react-router-dom';
+// styled components imports
+import styled from 'styled-components';
+
+// styled components
+const ArticleContainer = styled.div`
+    width: 100%;
+`
+
+const ArticleContent = styled.div`
+    width: 1200px;
+    margin: 0 auto;
+`
+
+const ArticlePin = styled.div`
+    background-color: green;
+    font-size: 1.25rem;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+    margin: 1rem 0;
+    padding: 0.5rem;
+    border-radius: 10px;
+    background-color: #2B2D42;
+    color: #fff;
+`
 
 class BoardView extends Component {
 
@@ -18,25 +46,30 @@ class BoardView extends Component {
   render() {
     console.log('CHECK STATE', this.state.myBoard)
     return (
-      <div>
-        {this.props.boards.map(board => {
-          if (board.articles) {
-            return (
-              <div>
-                {board.articles.map((article, articleIndex) => {
-                  if (article.board_id === parseInt(this.state.myBoard)) {
-                  return (
-                    <div key={`article = ${articleIndex}`}>
-                      {article.article_label}
-                    </div>
-                  );
-                  }
-                })}
-              </div>
-            );
-          }
-        })}
-      </div>
+      <ArticleContainer>
+        <ArticleContent>
+            {this.props.boards.map(board => {
+            if (board.articles) {
+                return (
+                <div>
+                    {board.articles.map((article, articleIndex) => {
+                    if (article.board_id === parseInt(this.state.myBoard)) {
+                    return (
+                        <ArticlePin key={`article = ${articleIndex}`}>
+                            <span>{article.article_label}</span>
+                            <a target="_blank" href={article.url.toString()}>{article.url}</a>
+                            <span>{article.board_id}</span>
+                            <button>Delete</button>
+                        </ArticlePin>
+                    );
+                    }
+                    })}
+                </div>
+                );
+            }
+            })}
+        </ArticleContent>
+      </ArticleContainer>
     );
   }
 };
